16b46bf02c2b86e89f81e3f77b81608240a7b0ec
Pablo C. Ruíz <pcruiher089@gmail.com>
2020-07-17 16:15:11 -0500

    ejercicio 2

---
+++ Clase3/Ejercicio2.py
@@ -0,0 +1,15 @@
+uno = int(input())
+dos = int(input())
+tres = int(input())
+
+if(uno > dos):
+    if(uno > tres):
+        print(uno)
+    else:
+        print(tres)
+else:
+    if(dos > tres):
+        print(dos)
+    else:
+        print(tres)
+
