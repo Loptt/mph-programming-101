2c5b79cbf817cb385a767e11bd77e4bb88fd0703
Carlos Estrada <carlos.deg02@hotmail.com>
2020-07-17 16:13:00 -0500

    Added extra clase 2

---
+++ Clase2/EjercicioEx3.py
@@ -0,0 +1,5 @@
+numero1 = int(input())
+numero2 = int(input())
+numero3 = int(input())
+
+print(numero1 < numero2 and numero2 < numero3)
---
+++ Clase2/EjercicioEx4.py
@@ -0,0 +1,6 @@
+numero1 = int(input())
+numero2 = int(input())
+numero3 = int(input())
+numero4 = int(input())
+
+print(numero1 < 0 and numero2 < 0 and numero3 < 0 and numero4 < 0)
