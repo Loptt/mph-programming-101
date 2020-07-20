90b3cdc575db887adc73c09d391fca2688595a9a
Pablo C. Ruíz <pcruiher089@gmail.com>
2020-07-20 16:03:17 -0500

    lower y upper bound

--- Clase3/EjercicioEx5.py
+++ Clase3/EjercicioEx5.py
@@ -1,10 +1,11 @@
 lado = float(input())
 diagonal = float(input())
-eps = 0.00001
+eps = 0.0001
 
-
-print( (2 * lado * lado)  ** 0.5)
-if diagonal - ( 2 * lado * lado ) ** ( 0.5 ) <= eps:
+#print( (2 * lado * lado)  ** 0.5)
+resta = diagonal - ( 2 * lado * lado ) ** ( 0.5 )
+#print(resta)
+if resta <= eps and resta >= -eps:
     print("es cuadrado")
 else:
     print("es rectangulo")
